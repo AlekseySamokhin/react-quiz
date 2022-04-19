8d192ff1a5d8f984ceab850d11d6f651678c1c2f
AlekseySamokhin <lel.dude01@gmail.com>
2022-04-19 14:13:50 +0300

    new branch

--- src/App.js
+++ src/App.js
@@ -6,21 +6,6 @@ import QuizList from "./containers/QuizList/QuizList";
 import Auth from "./containers/Auth/Auth";
 import QuizCreator from "./containers/QuizCreator/QuizCreator";
 
-//class App extends Component {
-//  render() {
-//    return (
-//      <Layout>
-//        <Switch>
-//          <Route path="/auth" component={Auth} />
-//          <Route path="/quiz-creator" component={QuizCreator} />
-//          <Route path="/quiz/:id" component={Quiz} />
-//          <Route path="/" component={QuizList} />
-//        </Switch>
-//      </Layout>
-//    );
-//  }
-//}
-
 function App() {
     return (
       <Layout>
